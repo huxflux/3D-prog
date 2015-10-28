5310e5d80bad4ed71becaab120b90480010cb730
huxflux <johnis@student.hist.no>
2015-10-28 14:53:40 +0100

    Bananas

--- DeezeBalls/src/mygame/Main.java
+++ DeezeBalls/src/mygame/Main.java
@@ -2,6 +2,7 @@ package mygame;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.light.DirectionalLight;
+import com.jme3.light.PointLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
@@ -9,6 +10,7 @@ import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
+import com.jme3.system.AppSettings;
 import java.util.ArrayList;
 
 /**
@@ -18,35 +20,59 @@ import java.util.ArrayList;
 public class Main extends SimpleApplication {
     public static void main(String[] args) {
         Main app = new Main();
+        
+        AppSettings newSetting = new AppSettings(true);
+        newSetting.setFrameRate(60);
+        app.setSettings(newSetting);
         app.start();
     }
     
+    Recording recording;
+    Spatial football;
+    float[] test;
+    int counter = 0;
     @Override
     public void simpleInitApp() {
-        String soccerFilename = ("src\\mygame\\soccerball.blend");
-        
         // Testing Coordinate class, it's aMaZzZiNg
-        Recording recording = new Recording("src\\mygame\\socket_2013011114_1513.dat");
-        System.out.println(recording.numberOfTimestamps());
-        System.out.println(recording.numberOfCoordinates());
-        for (int i = 0; i < recording.numberOfTimestamps(); i++) {
-            System.out.println(recording.getCoordinate(i, 0));
-        }
-        
+        recording = new Recording("src\\mygame\\socket_2013011113_2810.dat");
+        test = recording.getCoordinatesMarker(4);
+        System.out.println(recording.toString());
+        System.out.println(test.length);
+       
+        Box box1 = new Box(1,1,1);
+        Geometry blue = new Geometry("Box", box1);
+        blue.setLocalTranslation(new Vector3f(1,-1,1));
+        Material mat1 = new Material(assetManager, 
+                "Common/MatDefs/Misc/Unshaded.j3md");
+        mat1.setColor("Color", ColorRGBA.Blue);
+        blue.setMaterial(mat1);
+
         // Testing my hairy ball
-        Spatial football = assetManager.loadModel("Models/hairyBall/hairyBall.j3o");
+        football = assetManager.loadModel("Models/hairyBall/hairyBall.j3o");
         football.setLocalTranslation(0.0f, 0.0f, 0.0f);
-        football.scale(0.5f, 0.5f, 0.5f);
+        football.scale(1.0f, 1.0f, 1.0f);
+        cam.setLocation(new Vector3f(0.0f, 0.0f, 10.0f));
         //moor di!
         DirectionalLight sun = new DirectionalLight();
-        sun.setDirection(new Vector3f(-0.1f, -0.7f, -1.0f));
-        
+        sun.setDirection(new Vector3f(-0.1f, -0.7f, -2.0f));
+//        sun.setDirection(new Vector3f(0.0f, 0.0f, -2.0f));
+//        PointLight sun = new PointLight();
+//        sun.setPosition(Vector3f.ZERO);
+        flyCam.setZoomSpeed(50);
         rootNode.attachChild(football);
+        rootNode.attachChild(blue);
         rootNode.addLight(sun);
+        
     }
 
     @Override
     public void simpleUpdate(float tpf) {
+        football.setLocalTranslation(test[counter+0], test[counter+1], test[counter+2]);
+        counter += 3;
+        if (counter >= recording.getNumberOfTimestamps()*3) {
+            counter = 0;
+        }
+        
         //TODO: add update code
     }
 
